9b9906721b727fe0cc593c22aedd669f2e542e90
TelmenBay <bayarakh@gmail.com>
2025-11-11 02:22:05 -0500

    s

---
+++ next-simple/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import ProductCard from './products/ProductCard'
+
+export default function Home() {
+  return (
+    <main>
+      <h1>hi mom</h1>
+      <Link href='/users'>users list</Link>
+      <ProductCard/>
+    </main>
+  )
+}
