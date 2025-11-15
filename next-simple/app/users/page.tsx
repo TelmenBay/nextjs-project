55b3f718ab7893096e3bddbf9b0d94c042d4d8c4
TelmenBay <bayarakh@gmail.com>
2025-11-15 01:59:01 -0500

    static rendering fix

--- next-simple/app/users/page.tsx
+++ next-simple/app/users/page.tsx
@@ -6,7 +6,7 @@ interface User {
 }
 
 const UserPage = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', { cache: 'no-store'})
   const users: User[] = await res.json()
 
   return (
@@ -14,6 +14,7 @@ const UserPage = async () => {
       <h1>
         users
       </h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map(user => <li key={user.id}>{user.name}</li>)}
       </ul>
